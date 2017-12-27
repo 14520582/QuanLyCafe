0fed6a93650ebc95aaee23888b1e9e50766e251e
KimNgan96 <14520575@gm.uit.edu.vn>
2017-12-28 00:33:04 +0700

    update report

---
+++ QuanLyQuanCafe/VIEW/frmReport.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace QuanLyQuanCafe.VIEW
+{
+    partial class frmReport
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.Thang = new DevExpress.XtraEditors.ComboBoxEdit();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.Nam = new DevExpress.XtraEditors.TextEdit();
+            ((System.ComponentModel.ISupportInitialize)(this.Thang.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Nam.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(340, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Xem";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Thang
+            // 
+            this.Thang.EditValue = "";
+            this.Thang.Location = new System.Drawing.Point(66, 41);
+            this.Thang.Name = "Thang";
+            this.Thang.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.Thang.Properties.DropDownRows = 12;
+            this.Thang.Properties.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12"});
+            this.Thang.Size = new System.Drawing.Size(86, 20);
+            this.Thang.TabIndex = 1;
+            this.Thang.SelectedIndexChanged += new System.EventHandler(this.comboBoxEdit1_SelectedIndexChanged);
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Appearance.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl1.Appearance.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.labelControl1.Location = new System.Drawing.Point(13, 42);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(36, 16);
+            this.labelControl1.TabIndex = 2;
+            this.labelControl1.Text = "Tháng";
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Appearance.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl2.Appearance.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.labelControl2.Location = new System.Drawing.Point(168, 43);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(26, 16);
+            this.labelControl2.TabIndex = 3;
+            this.labelControl2.Text = "Năm";
+            // 
+            // Nam
+            // 
+            this.Nam.Location = new System.Drawing.Point(214, 40);
+            this.Nam.Name = "Nam";
+            this.Nam.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nam.Properties.Appearance.Options.UseFont = true;
+            this.Nam.Size = new System.Drawing.Size(83, 22);
+            this.Nam.TabIndex = 4;
+            // 
+            // frmReport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(451, 103);
+            this.Controls.Add(this.Nam);
+            this.Controls.Add(this.labelControl2);
+            this.Controls.Add(this.labelControl1);
+            this.Controls.Add(this.Thang);
+            this.Controls.Add(this.button1);
+            this.Name = "frmReport";
+            this.Text = "Báo cáo";
+            this.Load += new System.EventHandler(this.frmReport_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Thang.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Nam.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private DevExpress.XtraEditors.ComboBoxEdit Thang;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.TextEdit Nam;
+    }
+}
---
+++ QuanLyQuanCafe/VIEW/frmReport.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QuanLyQuanCafe.BUS;
+using QuanLyQuanCafe.DTO;
+using DevExpress.XtraPrinting;
+using DevExpress.XtraReports.UI;
+using DevExpress.XtraPrinting.Preview;
+namespace QuanLyQuanCafe.VIEW
+{
+    public partial class frmReport : Form
+    {
+        DateTime current = DateTime.Now;
+        String first, last;
+        DateTime firstDay, lastDay;
+        float total;
+        public frmReport()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(CheckInvalidMonth(Convert.ToInt32(Thang.Text), Convert.ToInt32(Nam.Text)) == false)
+            {
+                MessageBox.Show("Hiện chưa có dữ liệu của tháng đã chọn hoặc tháng được chọn là không đúng");
+            }
+            else
+            {
+                
+                
+                DataTable data = new DataTable();
+                first = "1/" + Thang.Text +"/"+ Nam.Text;
+                last = GetLastDayOfMonth(Convert.ToInt32(Thang.Text), Convert.ToInt32(Nam.Text)).ToString() +"/"+ Thang.Text +"/"+ Nam.Text;
+                firstDay = Convert.ToDateTime(first);
+                lastDay = Convert.ToDateTime(last);
+
+                data = Bill_BUS.FindBillByDate(firstDay, lastDay);
+                //data = Bill_BUS.LoadAllBill();
+                Report report = new Report();
+
+                List<Bill_DTO> billList = new List<Bill_DTO>();
+                for (int i = 0; i < data.Rows.Count; i++)
+                {
+                    Bill_DTO bill = new Bill_DTO();
+                    bill.Id = Convert.ToInt32(data.Rows[i]["IdBill"]);
+                    bill.Date = DateTime.Parse(data.Rows[i]["Date"].ToString());
+                    bill.Total = Convert.ToInt32(data.Rows[i]["TotalPrice"]);
+                    billList.Add(bill);
+                }
+                report.lbNgayLap.Text = current.Day.ToString() + "/" + current.Month.ToString() + "/" + current.Year.ToString();
+                report.lbthang.Text = Thang.Text + "/" + Nam.Text;
+                report.lbCountID.Text = data.Rows.Count.ToString();
+                report.lbSumPrice.Text = data.Compute("Sum(TotalPrice)","").ToString()+ " VNĐ";
+                report.lbntn.Text = "ngày " + current.Day.ToString() + " tháng " + current.Month.ToString() +" năm " + current.Year.ToString()+".";
+                try
+                {
+                    report.DataSource = billList;
+                    ReportPrintTool tool = new ReportPrintTool(report);
+                    tool.ShowPreview();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+            
+            
+
+        }
+
+        private void frmReport_Load(object sender, EventArgs e)
+        {
+            if (current.Month > 1)
+            {
+                Thang.Text = (current.Month - 1).ToString();
+                Nam.Text = current.Year.ToString();
+            }
+            else
+            {
+                Thang.Text = "12";
+                Nam.Text = (current.Year - 1).ToString();
+            }
+        }
+
+        private void comboBoxEdit1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Nam_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private int GetLastDayOfMonth(int month, int year)
+        {
+            if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+                return 31;
+            else
+            {
+                if (month == 2)
+                {
+                    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+                        return 29;
+                    else return 28;
+                }
+                else
+                    return 30;
+            }
+        }
+
+        private Boolean CheckInvalidMonth(int month, int year)
+        {
+            if (year > current.Year)
+                return false;
+            if (year == current.Year && month > current.Month)
+                return false;
+            return true;
+        }
+
+    }
+}

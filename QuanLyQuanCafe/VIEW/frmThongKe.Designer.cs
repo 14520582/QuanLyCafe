c5bf20ae78f396d93a20663d87f6faaab263265d
BaoTramuit <14520986@gm.uit.edu.vn>
2017-12-29 11:58:14 +0700

    tram 29/12

---
+++ QuanLyQuanCafe/VIEW/frmThongKe.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace QuanLyQuanCafe.VIEW
+{
+    partial class frmThongKe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmThongKe));
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnThongKe = new DevExpress.XtraEditors.SimpleButton();
+            this.cbxLoai = new System.Windows.Forms.ComboBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Thống kê";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.btnThongKe);
+            this.groupBox1.Controls.Add(this.cbxLoai);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 1);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(812, 84);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(258, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Chọn năm";
+            // 
+            // btnThongKe
+            // 
+            this.btnThongKe.Image = ((System.Drawing.Image)(resources.GetObject("btnThongKe.Image")));
+            this.btnThongKe.Location = new System.Drawing.Point(485, 22);
+            this.btnThongKe.Name = "btnThongKe";
+            this.btnThongKe.Size = new System.Drawing.Size(102, 25);
+            this.btnThongKe.TabIndex = 20;
+            this.btnThongKe.Text = "Thống kê";
+            this.btnThongKe.Click += new System.EventHandler(this.btnThongKe_Click);
+            // 
+            // cbxLoai
+            // 
+            this.cbxLoai.DisplayMember = "Trống";
+            this.cbxLoai.FormattingEnabled = true;
+            this.cbxLoai.Items.AddRange(new object[] {
+            "Doanh thu",
+            "Số lượng món"});
+            this.cbxLoai.Location = new System.Drawing.Point(95, 23);
+            this.cbxLoai.Name = "cbxLoai";
+            this.cbxLoai.Size = new System.Drawing.Size(131, 21);
+            this.cbxLoai.TabIndex = 17;
+            this.cbxLoai.Text = "Doanh thu";
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(32, 107);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(727, 335);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(340, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 22;
+            // 
+            // frmThongKe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(790, 454);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmThongKe";
+            this.Text = "Thống kê";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cbxLoai;
+        private DevExpress.XtraEditors.SimpleButton btnThongKe;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ QuanLyQuanCafe/VIEW/frmThongKe.cs
@@ -0,0 +1,83 @@
+﻿using DevExpress.XtraCharts;
+using QuanLyQuanCafe.DAO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyQuanCafe.VIEW
+{
+    public partial class frmThongKe : Form
+    {
+        public frmThongKe()
+        {
+            InitializeComponent();
+        }
+
+        private void btnThongKe_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text == "")
+            {
+                MessageBox.Show("Vui lòng nhập năm để thống kê", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                chart1.Series.Clear();
+                DataTable dtb = Bill_DAO.StatisticByRevenue(int.Parse(textBox1.Text));
+                if (dtb.Rows.Count > 0)
+                {
+                    chart1.Series.Add("Doanh thu");
+                    int str1, sl1;
+                    int j = 1;
+                    for (int i = 0; i < dtb.Rows.Count; i++)
+                    {
+                        str1 = (int)dtb.Rows[i][0];
+                        sl1 = (int)dtb.Rows[i][1];
+
+                        if (str1 == j)
+                        {
+                            chart1.Series["Doanh thu"].Points.AddXY(str1, sl1);
+                            j++;
+                        }
+                        else
+                        {
+
+                            for (; j < str1; j++)
+                            {
+                                chart1.Series["Doanh thu"].Points.AddXY(j, 0);
+                            }
+
+                            chart1.Series["Doanh thu"].Points.AddXY(str1, sl1);
+                            j++;
+
+                        }
+                    }
+                    if (j < 13)
+                    {
+                        for (; j < 13; j++)
+                        {
+                            chart1.Series["Doanh thu"].Points.AddXY(j, 0);
+                        }
+                    }
+                }
+                else
+                    MessageBox.Show("Không có dữ liệu để thống kê năm " + textBox1.Text, "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }            
+
+        }
+        private void ThongKe()
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
